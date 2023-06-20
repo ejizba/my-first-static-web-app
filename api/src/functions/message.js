42a9136b7019402bfeeeeff576eff937dce40bf7
Eric Jizba <erijiz@microsoft.com>
2023-06-20 11:57:46 -0700

    Add api

---
+++ api/src/functions/message.js
@@ -0,0 +1,10 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        return { body: `Hello from the API` };
+    }
+});
